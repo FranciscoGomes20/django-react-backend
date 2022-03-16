888530fe23aecd0cc92083ad77329b50cba5c0a6
FranciscoGomes20 <henriquecgomes10@gmail.com>
2022-03-15 22:41:11 -0500

    Adding token in project

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, authentication
+from .serializers import ListSerializer, ItemSerializer
+from .models import Item, List
 
-# Create your views here.
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [authentication.TokenAuthentication, authentication.SessionAuthentication]
